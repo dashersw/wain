aa8a76682ad373b1208a454eaa00209408948d9e
abdullah <writer-edi@hotmail.com>
2016-12-23 23:00:00 +0300

    Added HTML parser function

--- crawler/index.js
+++ crawler/index.js
@@ -1,8 +1,25 @@
 var async = require('async');
 var sources = require('./sources.json');
+var request = require('request');
+var htmlToText = require('html-to-text');
 
 function fetchArticlesFromSource(source) {
-    console.log(source);
+    source.map(handleSource)
+}
+/** 
+ * Each feed url handle by this function.
+ * @param url Link of Article Feed
+ * @returns {?}
+ */
+function handleArticleSource(url) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var text = htmlToText.fromString(body);
+            console.log(text)
+        } else {
+             // I don't have an idea about handle error
+        }
+    })
 }
 
 async.each(sources, fetchArticlesFromSource);
