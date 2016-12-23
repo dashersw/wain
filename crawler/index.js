b3d66e2d4cd4006629d20ba9a6d09ae13c93f5be
abdullah <writer-edi@hotmail.com>
2016-12-23 23:10:08 +0300

    Change function name

--- crawler/index.js
+++ crawler/index.js
@@ -4,7 +4,7 @@ var request = require('request');
 var htmlToText = require('html-to-text');
 
 function fetchArticlesFromSource(source) {
-    source.map(handleSource)
+    source.map(handleArticleSource)
 }
 /** 
  * Each feed url handle by this function.
@@ -12,9 +12,10 @@ function fetchArticlesFromSource(source) {
  * @returns {?}
  */
 function handleArticleSource(url) {
+    var text;
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var text = htmlToText.fromString(body);
+            text = htmlToText.fromString(body);
             console.log(text)
         } else {
              // I don't have an idea about handle error
